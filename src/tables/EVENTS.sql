09b9a3f3f439750168052004b3a342a12a96a79f
cinadia <73308097+cinadia@users.noreply.github.com>
2022-06-12 12:30:31 -0700

    Include user fkey

--- src/tables/EVENTS.sql
+++ src/tables/EVENTS.sql
@@ -8,7 +8,13 @@ CREATE TABLE IF NOT EXISTS public.events
     event_date date,
     event_name character varying COLLATE pg_catalog."default" NOT NULL,
     event_location character varying COLLATE pg_catalog."default",
-    CONSTRAINT events_pkey PRIMARY KEY (event_id)
+    user_id integer NOT NULL,
+    CONSTRAINT events_pkey PRIMARY KEY (event_id),
+    CONSTRAINT events_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
 )
 
 TABLESPACE pg_default;
