0320462a74ff29d51b310c57a3fdbca5048741ef
cinadia <73308097+cinadia@users.noreply.github.com>
2022-06-12 12:31:18 -0700

    Remove table scripts, leave only db script

---
+++ src/script/Database_Script.sql
@@ -0,0 +1,12 @@
+-- Database: network-app
+
+-- DROP DATABASE "network-app";
+
+CREATE DATABASE "network-app-test"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
