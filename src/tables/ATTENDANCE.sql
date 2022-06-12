a9de3a61fd0e6a5ef25578523dbed552c3c2f1b0
cinadia <73308097+cinadia@users.noreply.github.com>
2022-06-12 12:29:34 -0700

    Include contact fkey, event fkey

--- src/tables/ATTENDANCE.sql
+++ src/tables/ATTENDANCE.sql
@@ -7,5 +7,15 @@ CREATE TABLE IF NOT EXISTS public.attendance
     contact_id integer NOT NULL,
     event_id integer NOT NULL,
     notes character varying COLLATE pg_catalog."default",
-    CONSTRAINT attendance_pkey PRIMARY KEY (contact_id, event_id)
+    CONSTRAINT attendance_pkey PRIMARY KEY (contact_id, event_id),
+    CONSTRAINT attendance_contacts_fkey FOREIGN KEY (contact_id)
+        REFERENCES public.contacts (contact_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT attendance_events_fkey FOREIGN KEY (event_id)
+        REFERENCES public.events (event_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
 )
